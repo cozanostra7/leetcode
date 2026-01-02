b5039430b36f8198614435f3c8ea58ee48f55f3e
Bekhzod Muminov <begzodmuminovb@gmail.com>
2026-01-02 15:45:41 +0100

    Add findClosestNumber method in Solution class
    Implement findClosestNumber method to find the closest number to zero in a list.

---
+++ closest_to_zero.py
@@ -0,0 +1,13 @@
+#Find Closest Number to Zero
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+        for n in nums:
+            if abs(n)<abs(closest):
+                closest = n
+        if closest<0 and abs(closest) in nums:
+
+                return abs(closest)
+        else:
+            return closest
