6d57513167b9732d61f29fd23581825c7eb3b30a
Bekhzod Muminov <begzodmuminovb@gmail.com>
2026-01-02 15:41:17 +0100

    Implement containsDuplicate method in Solution class

---
+++ ContainsDuplicate.py
@@ -0,0 +1,10 @@
+#Contains Duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            else:
+                s.add(num)
